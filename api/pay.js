a0a4f6155075394876ee2723e1b57ceea4cbfa9d
aussiewebstudioz <aussiewebstudio.z@gmail.com>
2026-03-30 20:58:00 +0530

    Update pay.js

--- api/pay.js
+++ api/pay.js
@@ -7,22 +7,25 @@ export default function handler(req, res) {
     return res.status(400).send("Missing required parameters");
   }
 
-  // 
-  const returnUrl = "https://www.swingsbyryzu.com/thank_you";
-  const cancelUrl = "https://www.swingsbyryzu.com/orders/{order-id}/authenticate";
-  const notifyUrl = "https://payhere-backend-zm4p-gz3rp15y4-aussiewebstudioz-2297s-projects.vercel.app/api/payhere-webhook";
+  const returnUrl = "https://swingsbyryzu.com";
+  const cancelUrl = "https://swingsbyryzu.com";
+  const notifyUrl = "https://payhere-backend-zm4p-41pxjx4to-aussiewebstudioz-2297s-projects.vercel.app/api/payhere-webhook";
 
-  const payHereUrl =
-    "https://www.payhere.lk/pay/checkout?" +
-    "merchant_id=" + merchantId +
-    "&return_url=" + encodeURIComponent(returnUrl) +
-    "&cancel_url=" + encodeURIComponent(cancelUrl) +
-    "&notify_url=" + encodeURIComponent(notifyUrl) +
-    "&order_id=" + order +
-    "&items=" + encodeURIComponent("Order " + order) +
-    "&currency=LKR" +
-    "&amount=" + amount +
-    "&first_name=" + encodeURIComponent(name);
-
-  res.redirect(payHereUrl);
+  res.send(`
+    <html>
+      <body onload="document.forms[0].submit()">
+        <form method="POST" action="https://www.payhere.lk/pay/checkout">
+          <input type="hidden" name="merchant_id" value="${merchantId}" />
+          <input type="hidden" name="return_url" value="${returnUrl}" />
+          <input type="hidden" name="cancel_url" value="${cancelUrl}" />
+          <input type="hidden" name="notify_url" value="${notifyUrl}" />
+          <input type="hidden" name="order_id" value="${order}" />
+          <input type="hidden" name="items" value="Order ${order}" />
+          <input type="hidden" name="currency" value="LKR" />
+          <input type="hidden" name="amount" value="${amount}" />
+          <input type="hidden" name="first_name" value="${name}" />
+        </form>
+      </body>
+    </html>
+  `);
 }
